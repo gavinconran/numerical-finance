15dd3ca02564bd0e739c43fa8d1fcde7ea24b19b
gavin <gavinconran@yahoo.co.uk>
2016-08-30 09:28:31 +0100

    changed r=0 and sigma=1 in MonteCarloEx5.m

--- 02_MonteCarlo/MonteCarloEX14.m
+++ 02_MonteCarlo/MonteCarloEX14.m
@@ -30,18 +30,17 @@ for steps = Tsubs_List
     % Initialise the vector of Estimated Stock Price at time T
     Price_t = zeros(sims, length(time_steps));
     Price_t(:,1) = S0;
-    t = T / steps;
+    t = T / (steps+1);
     
     % Initialise the vector of Estimated Stoke Prices
     Est_Price_T = zeros(1, sims);
     num_sims = linspace(1,sims, sims);
     
-    Price_sum = S0;
+    Price_sum = 0; #S0;
     for i=1:sims #num_sims     
         % For each time step compute the Stock Price St 
         for j = 2:length(time_steps)         
             Price_t(i,j) = Price_t(i,j-1)*exp((r-sigma.^2/2)*t + sigma*sqrt(t)*normrnd(0,1));
-            #Price_t(i,j) = Price_t(i,j-1) + sqrt(t)*normrnd(r,sigma);
         endfor
         % Compute Estimator for i simulations
         Price_sum += Price_t(i,end);
@@ -58,7 +57,11 @@ str = sprintf('Convergence of Estimation wrt no. Simulated Paths');
 title(str); 
 ylim([50, 200]);
 
-if fig_num == 2
+%Plot histogram
+figure(2)
+hist(Price_t(:,end));
+
+if fig_num == 1
     price = Est_Price_T(end);
 else    
     price = max(Est_Price_T(end)-K,0);
@@ -72,9 +75,9 @@ r=0.05;
 sigma=0.20;
 T=1.0;
 Tsubs_List = [100]; #[100, 200, 1000];
-sims = 1000;
+sims = 10000;
 K=100;
-MonteCarloEstimator(S0, r, sigma, Tsubs_List, T, sims, K, 1);
+%MonteCarloEstimator(S0, r, sigma, Tsubs_List, T, sims, K, 1);
 
 %%% PART 3: with 1000 paths and 100 time subintervals, plot the function K → C 0 (K) for K = 20, . . . , 150.
 S0 = 100;
@@ -83,6 +86,6 @@ sigma=0.20;
 T=1.0;
 Tsubs_List = [100];
 sims = 1000;
-K=150;
+K=20;
 MonteCarloEstimator(S0, r, sigma, Tsubs_List, T, sims, K, 3)
 
