9890c7882e332f31613225dc8adc4d065217061f
gavin <gavinconran@yahoo.co.uk>
2016-08-31 14:04:19 +0100

    updated MonteCarloEx15.py - need a solution to dW component

--- 02_MonteCarlo/MonteCarloEx15.py
+++ 02_MonteCarlo/MonteCarloEx15.py
@@ -26,10 +26,7 @@ random.seed(1234)
 
 # function Euler step return the present solution plus a bit more
 def euler_step(U, dt, theta, sigma, K):
-
-    return U + dt*K*(theta-U) #+ sigma*np.sqrt(dt)*np.sqrt(abs(U)) *random.random()
-    #return U + dt*K*(theta-U) + sigma*np.sqrt(dt)*np.sqrt(max(U, 0)) *random.random()
-    #return U + dt*K*(theta-U) + sigma*np.sqrt(dt)*np.sqrt(U) *random.random()
+    return U + dt*K*(theta-U) + sigma*np.sqrt(dt)*np.sqrt(abs(U)) *random.random() 
 
 
 # Construct Grid
@@ -48,52 +45,83 @@ theta = 0.09
 sigma = 0.1
 K = 2.1
 
-# time loop - Euler method
-for n in range(0, N-1):
-    U[n+1] = euler_step(U[n], dt, theta, sigma, K)
+# Convergence of Bond Price as number of simulations become large
+# Plot trajetory of Bond Prices over time (* num_sims)
+pyplot.figure(figsize=(8,6))
 
-print("Approx. Price: ", U[-1])
+
+num_sims = 50
+sims = range(num_sims)
+B_n = np.zeros(num_sims)
+B_Price = B_0
+for i in sims:
+    # time loop - Euler method
+    for n in range(0, N-1):
+        U[n+1] = euler_step(U[n], dt, theta, sigma, K)
+    B_Price += U[-1]
+    pyplot.plot(t,U, 'k-', lw=2);
+    B_n[i] = B_Price / (i+1)
+
+pyplot.grid(True)
+pyplot.xlabel(r't', fontsize=18)
+pyplot.ylabel(r'Bond Price', fontsize=18)
+pyplot.title('Bond Price over time, dt: %.3f, #sims: %d' % (dt, num_sims), fontsize=18)
+pyplot.plot(t, U, 'k-', lw=2);
+pyplot.show()
+
+
+print("Approx. Price: ", B_n[-1])
 
 # Compute Analytic Price
 P_Analytic = analytic_B(K, sigma, theta,T, B_0)
 print("Analytic Price: ", P_Analytic)
 
-# Plot trajetory of Bond Prices over time
+
+# Plot Convergence of Bond Prices wrt no. simulations
 pyplot.figure(figsize=(8,6))
 pyplot.grid(True)
-pyplot.xlabel(r't', fontsize=18)
+pyplot.xlabel(r'# simulations', fontsize=18)
 pyplot.ylabel(r'Bond Price', fontsize=18)
-pyplot.title('Bond Price over time, delta t = %.3f' % dt, fontsize=18)
-pyplot.plot(t,U, 'k-', lw=2);
+pyplot.title('Convergence of Bond Price wrt # simulations', fontsize=18)
+sims = [x + 1 for x in sims]
+pyplot.plot(sims, B_n, 'k-', lw=2);
 pyplot.show()
 
 
+
 # Convergence of Bond Price as number of time steps become large
-N_List = range(102,5002, 10)
+N_List = range(2,5002, 1000)
 B = np.empty(len(N_List))
 t = np.linspace(0, T, len(N_List))
-index = 0          
+
+num_sims = 5
+sims = range(num_sims)
+
+sim = 0  
+# for each # steps        
 for N in N_List:
-    dt = T / (N-1)
-    #print('dt: ', dt)
-    U = np.empty(N)
+    dt = T / (N-1) # compute dt
+    U = np.empty(N) # Initialise U (Euler array)
     U[0] = 0.07
 
-    # time loop - Euler method
-    for n in range(0, N-1):
-        U[n+1] = euler_step(U[n], dt, theta, sigma, K)
-
-    B[index] = U[n+1]
-    index += 1
+    B_Price = 0
+    for i in sims: # rum simulations
+        # time loop - Euler method
+        for n in range(0, N-1):
+            U[n+1] = euler_step(U[n], dt, theta, sigma, K)
+        B_Price += U[-1]
+    B[sim] = B_Price / (i+1) # Expectation of Bond Price for this simulation
+    sim += 1
 
 # Plot convergence of Bond price as the number of time steps increases
 pyplot.figure(figsize=(8,6))
 pyplot.grid(True)
 pyplot.xlabel(r'number of time steps', fontsize=18)
 pyplot.ylabel(r'Bond Price', fontsize=18)
-pyplot.title('Convergence of Bond Price', fontsize=18)
+pyplot.title('Convergence of Bond Price wrt # time steps', fontsize=18)
 pyplot.plot(N_List,B, 'k-', lw=2);
 pyplot.show()
+
 
 
 
