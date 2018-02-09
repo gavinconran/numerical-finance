a2701935e5c13705b18d92be31bf4596b070e467
gavinconran <gtconran@gmail.com>
2018-02-09 14:58:39 +0000

    added plot_8-error.py scripts

--- 07_HighPerformanceComputing/part4/plot_mc_laplace_error.py
+++ 07_HighPerformanceComputing/part4/plot_mc_laplace_error.py
@@ -2,7 +2,7 @@
 from pylab import *
 
 # read in three columns from file and unpack into 3 arrays:
-n,int_approx,error = loadtxt('mc_quad_error.txt',unpack=True)
+n,int_approx,error = loadtxt('mc_laplace_error.txt',unpack=True)
 
 figure(1)
 clf()
@@ -11,5 +11,5 @@ loglog([1,1e7],[1,sqrt(1e-7)],'k',label='1 / sqrt(N)')
 legend()
 xlabel('number of MC points used')
 ylabel('abs(error)')
-title('Part 2: Log-log plot of relative error in MC quadrature')
-savefig('mc_quad_error.png')
+title('Part4: Log-log plot of relative error in MC Laplace Random Walk')
+savefig('mc_laplace_error.png')
